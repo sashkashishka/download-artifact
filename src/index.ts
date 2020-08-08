73267c1607fa4509fd73f4a472e9d838cb019a9f
Aleksandr Taperik <alexandr1696@hotmail.com>
2020-08-08 15:32:10 +0300

    feat: ts, rollup

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+// async function run() {
+  // try {
+//
+  // } catch(e) {
+//
+  // }
+// }
+
+console.log(core)
+console.log(github)
